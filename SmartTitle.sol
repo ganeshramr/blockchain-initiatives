282f8592fb17306901f3114d21ed83b400079e5d
ganehsramr <mailganesh@gmail.com>
2017-09-28 12:31:02 -0400

    version 2

--- SmartTitle.sol
+++ SmartTitle.sol
@@ -7,82 +7,103 @@ contract Automobile{
      string  year;
      uint numberOfAccidents;
      string[] historyOfAccidents;
+     uint price;
 }
 
 contract Car is Automobile{
-
-   //About the CAR
+    //About the CAR
     address public owner;
-    function  Car(string _make,string _model,string _year, uint _numberOfAccidents) public {
-      make = _make;
-      model = _model;
-      year = _year;
-      numberOfAccidents = _numberOfAccidents;
-   }
 
-    function carDetails() public constant returns (string,string,string,uint){
-      return (make,model,year,numberOfAccidents);
+    function  Car(string _make,string _model,string _year, uint _numberOfAccidents,uint _price) public {
+        make = _make;
+        model = _model;
+        year = _year;
+        price = _price;
+        numberOfAccidents = _numberOfAccidents;
+    }
+
+    function carDetails() public constant returns (string,string,string,uint,uint){
+        return (make,model,year,numberOfAccidents,price);
+    }
+
+    function buyCar(uint _price){
+      if(_price >= price){
+        owner = msg.sender;
+      }else {
+        revert();
+      }
     }
 
     function addAccident(string description) public {
-      historyOfAccidents.push(description);
-      numberOfAccidents = numberOfAccidents + 1;
+        historyOfAccidents.push(description);
+        numberOfAccidents = numberOfAccidents + 1;
     }
 
-  function  accidentDetails(uint accidentNo) public constant returns (string){
+    function  accidentDetails(uint accidentNo) public constant returns (string){
       return historyOfAccidents[accidentNo];
     }
 
-  function buyCar(){
-    owner = msg.sender;
-  }
 
-  //About the Lien
-   address lienHolder;
+    //About the Lien
+    address public lienHolder;
+    uint public outStandingLoanOnCar;
+    uint public monthlyDue;
+
+    mapping (address => LienDetails) public lienDetails;
 
     struct LienDetails {
       bool active;
       uint since;
-      uint256 loanAmount; //Ether
+      uint256 loanAmount; //Ether or $
     }
 
-    uint public outStandingLoanOnCar;
-
-
-    mapping (address => LienDetails) public lienDetails;
-
     function addLienholders(address _lienHolder) {
-
       lienDetails[_lienHolder] = LienDetails({active: true,since: now,loanAmount: 0});
       lienHolder = _lienHolder;
     }
 
-    function setDebt(uint _money){
+    function setDebt(uint _money, uint _monthlyDue){
 
         if(lienDetails[msg.sender].active){
-
-          lienDetails[msg.sender].loanAmount = _money;
-          outStandingLoanOnCar = outStandingLoanOnCar + _money;
-
-        }else{
+            lienDetails[msg.sender].loanAmount = _money;
+            outStandingLoanOnCar =  _money;
+            monthlyDue = _monthlyDue;
+        } else {
           revert();
         }
+    }
+
+    function reviseTotalDue(uint _money){
+
+      if(lienDetails[msg.sender].active){
+          outStandingLoanOnCar =  outStandingLoanOnCar - _money;
+          if(outStandingLoanOnCar == 0){
+            lienDetails[msg.sender].active = false;
+            lienHolder = address(0);
+          }
+      } else {
+        revert();
       }
 
-
+    }
 }
 
-contract DCU {
+//The BANK
+contract Bank {
 
     string public nameOfBank;
 
-    function DCU(string _name){
+    function Bank(string _name){
       nameOfBank = _name;
     }
 
-    function lend(address _borrowerAssetId, uint _money){
+    function lend(address _borrowerAssetId, uint _money, uint _loanPeriod){
       Car car = Car (_borrowerAssetId);
-      car.setDebt(_money);
+      car.setDebt(_money,_money/_loanPeriod);
     }
 
+    function payMonthlyDue(uint _dueAmount,address _borrowerAssetId){
+      Car car = Car (_borrowerAssetId);
+      car.reviseTotalDue(_dueAmount);
+    }
 }

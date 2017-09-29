a55fa1b1cf21a46c570329ce979db01e11476230
ganehsramr <mailganesh@gmail.com>
2017-09-29 17:12:16 -0400

    near final

--- SmartTitle.sol
+++ SmartTitle.sol
@@ -5,29 +5,42 @@ contract Automobile{
      string  make;
      string  model;
      string  year;
-     uint numberOfAccidents;
-     string[] historyOfAccidents;
+     string  VIN;
      uint price;
 }
 
 contract Car is Automobile{
     //About the CAR
     address public owner;
+    uint public numberOfAccidents;
+    uint public numberOfServices;
+    string[] public historyOfAccidents;
+    string[] public serviceRecords;
 
-    function  Car(string _make,string _model,string _year, uint _numberOfAccidents,uint _price) public {
+    function  Car(
+      string _make,
+      string _model,
+      string _year,
+      uint _price,
+      string _VIN,
+      address preferredLender) public {
         make = _make;
         model = _model;
         year = _year;
         price = _price;
-        numberOfAccidents = _numberOfAccidents;
+        owner = msg.sender;
+        VIN = _VIN;
+        addPreferredLender(preferredLender);
     }
 
-    function carDetails() public constant returns (string,string,string,uint,uint){
-        return (make,model,year,numberOfAccidents,price);
+    function carDetails() public constant returns (string,string,string,string,uint){
+        return (VIN,make,model,year,price);
     }
 
-    function buyCar(uint _price){
-      if(_price >= price){
+
+    function buyCar() payable {
+      if( (msg.value / 1000000000000000000) >= price){
+        owner.transfer(msg.value);
         owner = msg.sender;
       }else {
         revert();
@@ -39,15 +52,23 @@ contract Car is Automobile{
         numberOfAccidents = numberOfAccidents + 1;
     }
 
-    function  accidentDetails(uint accidentNo) public constant returns (string){
-      return historyOfAccidents[accidentNo];
+    function addServiceRecord(string description) public {
+        serviceRecords.push(description);
+        numberOfServices = numberOfServices + 1;
     }
-
 
     //About the Lien
     address public lienHolder;
     uint public outStandingLoanOnCar;
     uint public monthlyDue;
+
+    modifier ownerOnly(){
+      if(msg.sender != owner){
+        revert();
+      }else{
+        _;
+      }
+    }
 
     mapping (address => LienDetails) public lienDetails;
 
@@ -57,53 +78,74 @@ contract Car is Automobile{
       uint256 loanAmount; //Ether or $
     }
 
-    function addLienholders(address _lienHolder) {
-      lienDetails[_lienHolder] = LienDetails({active: true,since: now,loanAmount: 0});
-      lienHolder = _lienHolder;
+    function addPreferredLender(address _lienHolder) ownerOnly {
+        lienDetails[_lienHolder] = LienDetails({active: true,since: now,loanAmount: 0});
+
     }
 
-    function setDebt(uint _money, uint _monthlyDue){
+    function setDebt(uint _loan, uint _monthlyDue){
 
         if(lienDetails[msg.sender].active){
-            lienDetails[msg.sender].loanAmount = _money;
-            outStandingLoanOnCar =  _money;
+            lienDetails[msg.sender].loanAmount = _loan;
+            outStandingLoanOnCar =  _loan;
             monthlyDue = _monthlyDue;
+            lienHolder = msg.sender;
         } else {
           revert();
         }
     }
 
-    function reviseTotalDue(uint _money){
+    function reviseTotalDue(uint _money) internal{
 
-      if(lienDetails[msg.sender].active){
           outStandingLoanOnCar =  outStandingLoanOnCar - _money;
           if(outStandingLoanOnCar == 0){
             lienDetails[msg.sender].active = false;
             lienHolder = address(0);
           }
-      } else {
-        revert();
-      }
 
+    }
+
+    function payMonthlyDue() payable {
+      reviseTotalDue(msg.value / 1000000000000000000);
+      lienHolder.transfer(msg.value) ;
     }
 }
 
-//The BANK
-contract Bank {
+//The Loan Program
+contract LoanProgram {
 
-    string public nameOfBank;
+    string public nameOfLoan;
+    address private owner;
+    event FundsTransfered(address from, address to, uint quantity);
+    uint public loanAmountInEther;
 
-    function Bank(string _name){
-      nameOfBank = _name;
+    function LoanProgram(string _name)  payable{
+      nameOfLoan = _name;
+      owner = msg.sender;
+      FundsTransfered(msg.sender, this, msg.value);
     }
 
-    function lend(address _borrowerAssetId, uint _money, uint _loanPeriod){
+    function () payable {
+      FundsTransfered(msg.sender, this, msg.value);
+    }
+
+      modifier bankOnly(){
+        if(msg.sender != owner){
+          revert();
+        }else{
+          _;
+        }
+      }
+
+    function lend(uint _loanAmount,address _borrowerId,address _borrowerAssetId, uint _loanRepaymentPeriod) bankOnly {
+
+      _borrowerId.transfer(_loanAmount);
+      uint _loanAmountinEther = _loanAmount / 1000000000000000000;
+      loanAmountInEther = _loanAmountinEther;
       Car car = Car (_borrowerAssetId);
-      car.setDebt(_money,_money/_loanPeriod);
+      car.setDebt(loanAmountInEther,loanAmountInEther / _loanRepaymentPeriod);
     }
 
-    function payMonthlyDue(uint _dueAmount,address _borrowerAssetId){
-      Car car = Car (_borrowerAssetId);
-      car.reviseTotalDue(_dueAmount);
-    }
+
+
 }

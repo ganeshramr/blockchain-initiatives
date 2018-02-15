54183c750df5304f79bd78a2c9355d9cc1b6b520
ganehsramr <mailganesh@gmail.com>
2018-02-15 12:46:02 -0500

    lint error fix

--- Loanchain.sol
+++ Loanchain.sol
@@ -1,8 +1,9 @@
-pragma solidity ^0.4.11;
+pragma solidity 0.4.18;
 
-contract Applicant{
 
-  struct  ApplicantAddress  {
+contract Applicant {
+
+    struct  ApplicantAddress {
         string  street1;
         string  street2;
         string  city;
@@ -12,142 +13,146 @@ contract Applicant{
     }
 
     struct Application {
-          string lenderName;
-          address lenderAddress;
-          bool active;
-          uint appliedDate;
-      }
+        string lenderName;
+        address lenderAddress;
+        bool active;
+        uint appliedDate;
+    }
 
-     string  applicantName;
-     string  applicantSex;
-     string  applicantDOB;
-     ApplicantAddress physicalAddress;
-     int ssn;
-     int applicantIncome;
-     address signedBy;
-     mapping (address => Application) public applicationDetails;
-     event ApplicationAcknowledged(address from);
-     event PersonalInfoRead(address from);
-     address[] public myApplications;
+    string private applicantName;
+    string private applicantSex;
+    string  private applicantDOB;
+    ApplicantAddress private homeAdd;
+    int private ssn;
+    int private applicantIncome;
+    address private signedBy;
+    mapping (address => Application) public applicationDetails;
+    event ApplicationAcknowledged(address from);
+    event PersonalInfoRead(address from);
+    address[] public myApplications;
 
-     modifier lenderCallOnly(){
-       if(!applicationDetails[msg.sender].active){
-         revert();
-       }else{
-         _;
-       }
-     }
+    modifier lenderCallOnly() {
+        if (!applicationDetails[msg.sender].active) {
+            revert();
+        } else {
+            _;
+        }
+    }
 
-     function Applicant(string _applicantName,
-                         string _applicantSex,
-                         string _applicantDOB,
-                         string _street1,
-                         string _street2,
-                         string _city,
-                         string _zip,
-                         string _state,
-                         string _country,
-                         int _ssn,
-                         int _applicantIncome) public{
+    function Applicant(string _applicantName,
+                        string _applicantSex,
+                        string _applicantDOB,
+                        string _street1,
+                        string _street2,
+                        string _city,
+                        string _zip,
+                        string _state,
+                        string _country,
+                        int _ssn,
+                        int _applicantIncome) public {
 
-        applicantName =  _applicantName;
+        applicantName = _applicantName;
         applicantSex = _applicantSex;
         applicantDOB = _applicantDOB;
-        physicalAddress = ApplicantAddress(_street1, _street2, _city, _zip,_state,_country);
+        homeAdd = ApplicantAddress(_street1, _street2, _city, _zip, _state, _country);
         ssn = _ssn;
         applicantIncome = _applicantIncome;
         signedBy = msg.sender;
-     }
+    }
 
-     function findBySSN(int _ssn) public constant returns (bool){
-       if(ssn == _ssn){
-         return true;
-       }
-       return false;
-     }
+    function findBySSN(int _ssn) public view returns (bool) {
+        if (ssn == _ssn) {
+            return true;
+        }
+        return false;
+    }
 
-     function ackApplication(string _name,address _lenderAddress) public{
-       applicationDetails[msg.sender] = Application(_name,_lenderAddress,true,now);
-       ApplicationAcknowledged(msg.sender);
-       myApplications.push(msg.sender);
-     }
+    function ackApplication(string _name, address _lenderAddress) public {
 
-     //add modified lenderCallOnly to restrict access ONLY to lender
-     function getApplicantDetails() public constant  returns(string,string,string,int,int,address){
-       return (applicantName,applicantSex,applicantDOB,ssn,applicantIncome,signedBy);
-     }
+        applicationDetails[msg.sender] = Application(_name, _lenderAddress, true, now);
+        ApplicationAcknowledged(msg.sender);
+        myApplications.push(msg.sender);
+    }
 
-     //add modified lenderCallOnly to restrict access ONLY to lender
-     function getApplicantAddress() public  constant  returns(string,string,string,string,string,string){
-       return(physicalAddress.street1,physicalAddress.street2,physicalAddress.city,physicalAddress.zip,physicalAddress.state,physicalAddress.country);
-     }
+       //add modified lenderCallOnly to restrict access ONLY to lender
+    function getApplicantDetails() public view  returns(string, string, string, int, int, address) {
+        return (applicantName, applicantSex, applicantDOB, ssn, applicantIncome, signedBy);
+    }
+
+       //add modified lenderCallOnly to restrict access ONLY to lender
+    function getApplicantAddress() public  view  returns(string, string, string, string, string, string) {
+        return(homeAdd.street1, homeAdd.street2, homeAdd.city, homeAdd.zip, homeAdd.state, homeAdd.country);
+    }
 }
+
 
 contract LoanProgram {
 
-  event ApplicationCreated(address contractAddress);
-  string public name;
-  function LoanProgram(string _name) public {
+    event ApplicationCreated(address contractAddress);
+    string public name;
 
-    name = _name;
-  }
+    function LoanProgram(string _name) public {
 
-  function apply(address _applicant,string _loanType, int _loanAmount,int _loanPeriodInYears) public {
+        name = _name;
+    }
 
-    address newContract = new Loan(name,_applicant,_loanType,_loanAmount,_loanPeriodInYears);
-    ApplicationCreated(newContract);
-  }
+    function apply(address _applicant, string _loanType, int _loanAmount, int _loanPeriodInYears) public {
+
+        address newContract = new Loan(name, _applicant, _loanType, _loanAmount, _loanPeriodInYears);
+        ApplicationCreated(newContract);
+    }
 }
+
 
 contract Loan {
 
-   address public applicantContractAddress;
-   string public loanType;
-   int public loanAmount;
-   address public loanProgramAddress = msg.sender;
-   bool public received;
-   bool public goodCredit;
-   bool public approved;
-   event UpdatingCreditStatusFor(int ssn);
-   event DisclosuresUpdated(int estimatedIntrestRate,int estimatedEMI);
-   event LoanAmountTxfed(uint amount);
-   int ssn;
-   int applicantIncome;
-   address signedBy;
-   int public estimatedIntrestRate;
-   int public estimatedEMI;
-   int public loanPeriodInYears;
+    address public applicantContractAddress;
+    string public loanType;
+    int public loanAmount;
+    address public loanProgramAddress = msg.sender;
+    bool public received;
+    bool public goodCredit;
+    bool public approved;
+    event UpdatingCreditStatusFor(int ssn);
+    event DisclosuresUpdated(int estimatedIntrestRate, int estimatedEMI);
+    event LoanAmountTxfed(uint amount);
+    int private ssn;
+    int private applicantIncome;
+    address private signedBy;
+    int public estimatedIntrestRate;
+    int public estimatedEMI;
+    int public loanPeriodInYears;
 
-   function Loan(string _programName,address _applicantContract,string _loanType,int _loanAmount,int _loanPeriodInYears) public {
-     Applicant applicant =  Applicant(_applicantContract);
-     applicant.ackApplication(_programName,loanProgramAddress);
-     (,,,ssn,applicantIncome,signedBy) = applicant.getApplicantDetails();
-     applicantContractAddress = _applicantContract;
-     loanType = _loanType;
-     loanAmount = _loanAmount;
-     received = true;
-     loanPeriodInYears = _loanPeriodInYears;
-   }
-
-   function updateCreditStatus(bool _creditStatus) public {
-      UpdatingCreditStatusFor(ssn);
-      goodCredit = _creditStatus;
-   }
-
-   function addDisclosure(int _estimatedIntrestRate,int _estimatedEMI) public {
-      estimatedIntrestRate = _estimatedIntrestRate;
-      estimatedEMI = _estimatedEMI;
-      DisclosuresUpdated(_estimatedIntrestRate,_estimatedEMI);
-
-   }
-
-  function approveLoan() public payable {
-    if(goodCredit){
-      signedBy.transfer(msg.value);
-      approved = true;
-      LoanAmountTxfed(msg.value);
-    }else{
-      revert();
+    function Loan(string _name, address _applicantContract, string _type, int _amount, int _periodInYears) public {
+        Applicant applicant =  Applicant(_applicantContract);
+        applicant.ackApplication(_name, loanProgramAddress);
+        (, , , ssn, applicantIncome, signedBy) = applicant.getApplicantDetails();
+        applicantContractAddress = _applicantContract;
+        loanType = _type;
+        loanAmount = _amount;
+        received = true;
+        loanPeriodInYears = _periodInYears;
     }
-  }
+
+    function updateCreditStatus(bool _creditStatus) public {
+        UpdatingCreditStatusFor(ssn);
+        goodCredit = _creditStatus;
+    }
+
+    function addDisclosure(int _estimatedIntrestRate, int _estimatedEMI) public {
+        estimatedIntrestRate = _estimatedIntrestRate;
+        estimatedEMI = _estimatedEMI;
+        DisclosuresUpdated(_estimatedIntrestRate, _estimatedEMI);
+
+    }
+
+    function approveLoan() public payable {
+        if (goodCredit) {
+            signedBy.transfer(msg.value);
+            approved = true;
+            LoanAmountTxfed(msg.value);
+        } else {
+            revert();
+        }
+    }
 }
